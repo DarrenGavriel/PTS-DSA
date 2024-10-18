f8b35389d993d571b8d8e70018706079ca7708a9
DarrenGavriel <darrengvs.dg@gmail.com>
2024-10-18 15:49:32 +0700

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -11,7 +11,7 @@ public class MyLinkedList {
             head=baru;
         }
     }
-	public void nambah_tengah(Node baru, String setelah) {
+	public void add(Node baru, String setelah){
 		if (head == null) {
 			add(baru);
 		}
@@ -28,24 +28,73 @@ public class MyLinkedList {
 			baru.setNext(temp.getNext());
 			temp.setNext(baru);
 		}
+    }
+	public void add_back(Node baru) {
+		if(tail==null) {
+			tail=baru;
+			head=baru;
+		}
+		else {
+			baru.setPrev(tail);
+			tail.setNext(baru);
+			tail=baru;
+		}
 	}
-	public void rm_mid(String judul) {
+	public void hapus() {
+		if (head == null) {
+			System.out.println("Data kosong");
+		}
+		else {
+			Node hapus = head;
+			head = head.getNext();
+			head.setPrev(null);
+			hapus = null;
+		}
+	}
+	public void hapus(String judul) {
 		Node temp = head;
+		if (head.getData().getNama().equals(judul)) {
+			hapus();
+		}
+		else if(tail.getData().getNama().equals(judul)) {
+			hapus_akhir();
+		}
 		while (temp != null) {
 			if(temp.getNext() == null) {
 				System.out.println("data rk nemu");
 				break;
 			}
 			else if (temp.getNext().getData().getNama().equals(judul)) {
+				Node hapus = temp.getNext();
+				temp.setNext(hapus.getNext());
+				hapus.getNext().setPrev(temp);
+				hapus = null;
 				break;
 			}
 			temp = temp.getNext();
 		}
-		Node hapus = temp.getNext();
-		temp.setNext(hapus.getNext());
-		hapus.getNext().setPrev(temp);
-		hapus = null;
 	}
+	public void hapus_akhir() {
+		if (tail == null) {
+			System.out.println("data kosong");
+		}
+		else {
+			Node hapus = tail;
+			tail = tail.getPrev();
+			tail.setNext(null);
+			hapus = null;
+		}
+	}
+	
+	public void merge(MyLinkedList l2) {
+		if(this.head == null || l2.head == null) {
+			return;
+		}
+		this.tail.setNext(l2.head);
+		l2.head.setPrev(this.tail);
+		this.tail = l2.tail;
+	}
+	
 	public void cetaklist(){
         Node temp = head;
         while(temp != null){
