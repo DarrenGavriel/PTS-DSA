db8f02315d7b9aa4990f7430de17ed7033f14dc6
Oilla <darleneangelina16@gmail.com>
2024-10-18 16:30:42 +0700

    Update TestDrive.java

--- TestDrive.java
+++ TestDrive.java
@@ -1,51 +1,47 @@
-package coba;
+package cobawen;
 
 public class TestDrive {
-		public static void main(String[]args) {
-			LinkedList ll = new LinkedList();
-			ll.addHead(new Node("Bohemian Rhapsody","Queen", 300,null,null));
-			ll.addHead(new Node("Melted","AKMU",350,null,null));
-			ll.addTail(new Node("Roly Poly","T-Ara",250,null,null));
-			ll.addTail(new Node("Rewrite The Stars","James Arthur & Anne Marie",400,null,null));
-			
-			System.out.println("List Awal:\n");
-			ll.cetakList();
-			
-			System.out.println("\n\nCETAK BELAKANG:");
-			ll.cetakBelakang();
-			
-			ll.addMid("Melted", new Node("Nxde","G-Idle",250,null,null));
-			ll.addMid("Roly Poly", new Node("In The End","Linkin Park",450,null,null));
-			ll.addMid("In The End", new Node("Dinosaur","AKMU",200,null,null));
-			ll.addMid("Nxde", new Node("Night Changes","One Direction",200,null,null));
-			ll.addMid("Bohemian Rhapsody", new Node("Give Love","AKMU",200,null,null));
-			
-			System.out.println("\n\nList Tambah Tengah:");
-			ll.cetakList();
-			
-			System.out.println("\n\nCETAK BELAKANG:");
-			ll.cetakBelakang();
-			
-//			System.out.println("\n\nList Hapus Head:");
-//			ll.removeHead();
-//			ll.cetakList();
-//			
-//			System.out.println("\n\nList Hapus Tail:");
-//			ll.removeTail();
-//			ll.cetakList();
-//			
-//			System.out.println("\n\nList Hapus Tengah:");
-//			ll.removeMid("MAGO");
-//			ll.cetakList();
-//			
-//			System.out.println("\n\nList setelah hapus tengah:");
-//			ll.cetakBelakang();
-			
-			System.out.println("\n\nMencari penyanyi: ");
-			ll.cariPenyanyi("AKMU");
-			System.out.println("\n");
-			
-			ll.cariPenyanyi("Nxde");
-			
-		}
+	public static void main(String[]args) {
+		LinkedList ll = new LinkedList();
+		ll.add(new Node("Bohemian Rhapsody","Queen", 300,null,null),"BACK");
+		ll.add(new Node("Melted","AKMU",350,null,null),"frONt");
+		ll.add(new Node("Roly Poly","T-Ara", 400, null, null),"BAck");
+		ll.add(new Node("Reality", "AKMU",500, null, null),"back");
+		
+		System.out.println("List Awal:\n");
+		ll.printList();
+		
+		System.out.println("\n\nCETAK BELAKANG:");
+		ll.printBehind();
+		
+		ll.add("Roly Poly",new Node("Night Changes", "One Direction", 300, null, null));
+		
+		System.out.println("\n\nADD IN THE MIDDLE: ");
+		ll.printList();
+		
+		System.out.println("\n\nPRINT FROM BEHIND");
+		ll.printBehind();
+
+		//Mencari lagu berdasarkan penyanyi
+		System.out.println("\n\n");
+		ll.findData("AKMU");
+		//Mencari lagu berdasarkan judul lagu
+		System.out.println("\n");
+		ll.findData("Nxde");
+		//Mencari data lagu berdasarkan durasi lagu
+		System.out.println("\n");
+		ll.findData(300);
+		
+		System.out.println("\n");
+		ll.moveData("Night Changes", "Bohemian Rhapsody");
+		ll.printList();
+		
+		System.out.println("\nRemoving Data Head or Tail or Middle:");
+		ll.remove("depan");
+		ll.remove("Roly Poly");
+		ll.remove("tail");
+		ll.printList();
+		
+		
+	}
 }
