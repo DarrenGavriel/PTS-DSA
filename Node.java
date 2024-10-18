8f1b06fe77e648a44e9dac116814c123f30b7eb4
DarrenGavriel <darrengvs.dg@gmail.com>
2024-10-18 17:43:50 +0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,36 +1,63 @@
-public class Node {
-	private Node next, prev;
-	private Data data;
-	public Node() {
-		data = null;
-		next = null;
-		prev = null;
-	}
-	public Node(Data d, Node n, Node p) {
-		next = n;
-		prev = p;
-		data = d;
-	}
-	public void setNext(Node n) {
-		next = n;
-	}
-	public Node getNext() {
-		return next;
-	}
-	public void setPrev(Node p) {
-		prev = p;
-	}
-	public Node getPrev() {
-		return prev;
-	}
-	public void setData(Data d) {
-		data = d;
-	}
-	public Data getData() {
-		return data;
-	}
-	// untuk mencetak data dari memanggil metode cetak data melalui referense data
-	public void cetak() {
-		data.cetakData();	
-	}
+public class Node{
+    private String judulLagu;
+    private String penyanyiLagu;
+    private int tahunRilis;
+    private int durasiLagu;
+    private Node next;
+
+    public Node(String judul, String penyanyi, int tahun, int durasi, Node n){
+        judulLagu = judul;
+        penyanyiLagu = penyanyi;
+        tahunRilis = tahun;
+        durasiLagu = durasi;
+        next = n;
+    }
+
+    public void setJudulLagu(String judul){
+        judulLagu = judul;
+    }
+
+    public String getJudulLagu(){
+        return judulLagu;
+    }
+
+    public void setPenyanyiLagu(String penyanyi){
+        penyanyiLagu = penyanyi;
+    }
+
+    public String getPenyanyiLagu(){
+        return penyanyiLagu;
+    }
+
+    public void setTahunRilis(int tahun){
+        tahunRilis = tahun;
+    }
+
+    public int getTahunRilis(){
+        return tahunRilis;
+    }
+
+    public void setDurasiLagu(int durasi){
+        durasiLagu = durasi;
+    }
+
+    public int getDurasiLagu(){
+        return durasiLagu;
+    }
+
+    public void setNext(Node n){
+        next = n;
+    }
+   
+    public Node getNext(){
+        return next;
+    }
+   
+    public void cetak(){
+        System.out.println("Judul Lagu: " + judulLagu + ", " + "Penyanyi: " + penyanyiLagu + ", " + "Tahun Rilis: " + tahunRilis + ", " + "Durasi Lagu: " + durasiLagu + " detik ");
+        //System.out.println("Penyanyi: " + penyanyiLagu + ",");
+        //System.out.println("Tahun Rilis: " + tahunRilis + ",");
+        //System.out.println("Durasi Lagu: " + durasiLagu + " detik ");
+        //System.out.print("\n");
+    }
 }
