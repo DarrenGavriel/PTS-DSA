f53fbe5b7a3b1abdba4d510fa925f20f2c3b9e7b
Oilla <darleneangelina16@gmail.com>
2024-10-18 16:30:15 +0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,62 +1,63 @@
-package coba;
+package cobawen;
 
 public class Node {
-		private String judul, penyanyi;
-		private int durasi;
-		private Node next, prev;
-		
-		public Node() {
-			judul = " ";
-			penyanyi=" ";
-			durasi= 0;
-			next=null;
-			prev=null;
-		}
-		
-		public Node(String j, String p, int d, Node n, Node pr) {
-			judul = j;
-			penyanyi = p;
-			durasi = d;
-			next = n;
-			prev = pr;	
-		}
-		
-		public void setJudul(String j) {
-			judul = j;
-		}
-		public void setPenyanyi(String p) {
-			penyanyi = p;
-		}
-		public void setDurasi(int d) {
-			durasi = d;
-		}
-		public void setNext(Node n) {
-			next = n;
-		}
-		public void setPrev(Node p) {
-			prev = p;
-		}
-		
-		public String getJudul() {
-			return judul;
-		}
-		public String getPenyanyi() {
-			return penyanyi;
-		}
-		public int getDurasi() {
-			return durasi;
-		}
-		public Node getNext() {
-			return next;
-		}
-		public Node getPrev() {
-			return prev;
-		}
-		
-		public void cetak() {			
-			String format="%-20s %-25s %-3ds %n";
-			System.out.format(format, judul, penyanyi, durasi);
-		}
+	private String title, singer;
+	private int duration;
+	private Node next, prev;
+	
+	public Node() {
+		title = " ";
+		singer =" ";
+		duration = 0;
+		next=null;
+		prev=null;
+	}
+	
+	public Node(String t, String s, int d, Node n, Node pr) {
+		title = t;
+		singer = s;
+		duration = d;
+		next = n;
+		prev = pr;	
+	}
+	
+	public void setTitle(String t) {
+		title = t;
+	}
+	public void setSinger(String s) {
+		singer = s;
+	}
+	public void setDuration(int d) {
+		duration = d;
+	}
+	public void setNext(Node n) {
+		next = n;
+	}
+	public void setPrev(Node p) {
+		prev = p;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getSinger() {
+		return singer;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public Node getPrev() {
+		return prev;
+	}
+	
+	public void print() {			
+		String format="%-20s %-28s %-3ds %n";
+		System.out.format(format, title, singer, duration);
+	}
 }
+
 
 
