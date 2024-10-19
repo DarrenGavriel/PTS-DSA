693a7d74c4cf1d46f2496bc8f4e5f6b6a4468509
DarrenGavriel <darrengvs.dg@gmail.com>
2024-10-19 14:35:20 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,27 @@
 public class Main {
 
 	public static void main(String[] args) {
-		MyLinkedList coba = new MyLinkedList();
-		coba.add(new Node(new Data("urutan 1", 123), null, null));
-		coba.add(new Node(new Data("urutan 2", 456), null, null));
-		coba.add(new Node(new Data("urutan 3", 789), null, null));
-		coba.add(new Node(new Data("urutan 4", 111), null, null));
-		coba.add(new Node(new Data("urutan 5", 222), null, null));
-		coba.add(new Node(new Data("ini tambah tengah", 999), null, null), "urutan 3");
-		coba.cetaklist();
+		Playlist coba = new Playlist();
+		coba.add(new Node(new Data("Like that", "Metro Boomin", 267), null, null));
+		coba.add(new Node(new Data("Superhero", "Metro Boomin", 182), null, null));
+		coba.add(new Node(new Data("LILAC", "IU", 214), null, null));
+		coba.add(new Node(new Data("Starlight", "Dave", 212), null, null));
+		coba.add(new Node(new Data("Eight", "IU", 167), null, null));
+//		coba.add(new Node(new Data("Reckless", "Madison Beer", 203), null, null), "Like ");
+		coba.add(new Node(new Data("Cookie", "NewJeans", 200), null, null));
+		coba.add(new Node(new Data("Celebrity", "IU", 242), null, null), "Starlight");
+
+		coba.cetaklist();// ini list asli
 		System.out.println();
 
-		MyLinkedList l2 = new MyLinkedList();
-		l2.add(new Node (new Data("ariana", 101), null, null));
-		l2.add(new Node (new Data("bryan", 102), null, null));
+		Playlist l2 = new Playlist();
+		l2.add(new Node (new Data("Mask Off", "Future", 204), null, null));
+		l2.add(new Node (new Data("Tore Up", "Don Toliver", 126), null, null));
 		
-		coba.merge(l2);
-		coba.cetaklist();
+		l2.cetaklist();// ini print l2
+		l2.merge(coba);
 		System.out.println();
-		coba.cetaklist_akhir();
-		coba.findData("ariana");
+		l2.cetaklist();// hasil merge sama l2
+		System.out.println();
 	}
 }
